553f44f69d8f3af872a7793b3b1b0aacd9288960
savic116 <savic00085@gmail.com>
2025-09-06 01:17:00 +0900

    feat: Print the result of the reduce function in 5_reduce_function.dart

--- lib/week01/1.4.1/5_reduce_function.dart
+++ lib/week01/1.4.1/5_reduce_function.dart
@@ -2,4 +2,6 @@ void main(){
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
   final allMembers = blackPinkList.reduce((value, element)=> value + ', ' + element);
+
+  print(allMembers);
 }
