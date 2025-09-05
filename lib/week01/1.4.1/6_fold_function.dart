7c415b9428be8bc992764f2517aa468cd82bdcf4
savic116 <savic00085@gmail.com>
2025-09-06 01:36:00 +0900

    feat(week01): Calculate total name length using fold function

--- lib/week01/1.4.1/6_fold_function.dart
+++ lib/week01/1.4.1/6_fold_function.dart
@@ -2,5 +2,7 @@ void main(){
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
 
-  final allMembers =blackPinkList
+  final allMembers =blackPinkList.fold<int>(0, (value, element) => value + element.length);
+
+  print(allMembers);
 }
