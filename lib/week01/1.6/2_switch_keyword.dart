742fa9d0fb46a25aaf156c3950508f4ec1deb41e
savic116 <savic00085@gmail.com>
2025-09-06 18:52:02 +0900

    Fix: Corrected switch statement syntax and added default case

--- lib/week01/1.6/2_switch_keyword.dart
+++ lib/week01/1.6/2_switch_keyword.dart
@@ -8,14 +8,19 @@ void main(){
   Status status = Status.approved;
 
   switch (status){
-    case Status.approved;
+    case Status.approved:
     // aproved값이기 때문에 다음 코드가 실행됩니다.
      print('승인 상태입니다.');
      break;
-    case Status.pending;
+    case Status.pending:
      print('대기 상태입니다.');
      break;
-    case Status.rejected;
+    case Status.rejected:
+     print('거절 상태입니다.');
+     break;
+    default:
+     print('알 수 없는 상태입니다.');
+     
      
   }
 }
