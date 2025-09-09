b6690bf1c2510133bed2702402d0201b855258f8
savic116 <savic00085@gmail.com>
2025-09-09 14:07:22 +0900

    feat: Implement GirlGroup class inheriting from Idol

--- lib/week02/2.4/1_override.dart
+++ lib/week02/2.4/1_override.dart
@@ -1,4 +1,22 @@
+class Idol {
+  final String name;
+  final int membersCount;
+
+  Idol(this.name, this.membersCount);
+
+  void sayName(){
+    print('저는 ${this.name}입니다.');
+  }
+
+
+  void sayMembersCount(){
+    print('${this.name} 맴버는 ${this.membersCount}명입니다.');
+  }
+}
 class GirlGroup extends Idol{
   // 2.3 상속에서처럼 super 키워드를 사용해도 되고 다음처럼 생성자의 매개변수로
   // 직접 super 키워드를 사용해도 됩니다.
+  GirlGroup(
+    
+  )
 }
