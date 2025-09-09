6b1051e39858df58fcaad5e73e3ffb28e8bafeed
savic116 <savic00085@gmail.com>
2025-09-09 14:05:22 +0900

    feat: Demonstrate inheritance and method calls in dart

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -35,4 +35,8 @@ void main(){
 
   BoyGroup bts = BoyGroup('BTS', 7); // 생성자로 객체 생성
 
+  bts.sayName(); // 부모한테 물려받은 메서드
+  bts.sayMembersCount(); // 부모한테 물려받은 메서드
+  bts.sayMale(); // 자식이 새로 추가한 메서드
+
 }
