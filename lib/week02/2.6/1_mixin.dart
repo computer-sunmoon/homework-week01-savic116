4cbc9d7a13bc10982ba9d5100c44ec938df07532
savic116 <savic00085@gmail.com>
2025-09-09 15:06:53 +0900

    Fix: Correct syntax errors in IdolSingMixin and BoyGroup classes

--- lib/week02/2.6/1_mixin.dart
+++ lib/week02/2.6/1_mixin.dart
@@ -18,17 +18,17 @@ mixin IdolSingMixin on Idol{
   void sing(){
     print('${this.name}이 노래를 부릅니다.');
   }
-
+}
   // 믹스인을 적용할 때는 with 키워드 사용
   class BoyGroup extends Idol with IdolSingMixin{
     BoyGroup(super.name, super.membersCount,);
-  };
+  }
 
   void sayMale(){
     print('저는 남자 아이돌입니다.');
   }
 
-}
+
 
 void main(){
     BoyGroup bts = BoyGroup('BTS', 7);
