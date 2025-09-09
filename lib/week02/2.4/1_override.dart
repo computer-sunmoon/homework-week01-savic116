ed465ed7976d307ff144f01823b194a5dbbbed36
savic116 <savic00085@gmail.com>
2025-09-09 14:11:22 +0900

    feat: Add comment explaining inherited method call in override example

--- lib/week02/2.4/1_override.dart
+++ lib/week02/2.4/1_override.dart
@@ -33,5 +33,8 @@ void main(){
 
   blackPink.sayName(); // 자식 클래스의 오버라이드된 메서드 사용
 
-  // say
+  // sayMembersCount는 오버라이드하지 않았기 때문에
+  // 그대로 Idol 클래스의 메서드가 실행됩니다.
+  // 부모 클래스의 메서드 사용
+  blackPink.sayMembersCount();
 }
