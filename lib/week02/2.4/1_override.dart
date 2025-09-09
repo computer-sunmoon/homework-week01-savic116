9fd92d9eb136ec0876aeb274192b1ea2d9a58692
savic116 <savic00085@gmail.com>
2025-09-09 14:08:22 +0900

    Fix: Pass parameters to Idol constructor in GirlGroup

--- lib/week02/2.4/1_override.dart
+++ lib/week02/2.4/1_override.dart
@@ -17,6 +17,11 @@ class GirlGroup extends Idol{
   // 2.3 상속에서처럼 super 키워드를 사용해도 되고 다음처럼 생성자의 매개변수로
   // 직접 super 키워드를 사용해도 됩니다.
   GirlGroup(
-    
-  )
+    super.name,
+    super.membersCount,
+  );
+
+  // override 키워드를 사용해 오버라이드합니다.
+
+  
 }
