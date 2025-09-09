44cfe2a9f0ae3d0038798671013fb4e15ebaca64
savic116 <savic00085@gmail.com>
2025-09-09 15:04:53 +0900

    Fix: Corrected BoyGroup constructor and added example usage.

--- lib/week02/2.6/1_mixin.dart
+++ lib/week02/2.6/1_mixin.dart
@@ -7,8 +7,16 @@ mixin IdolSingMixin on Idol{
 
   // 믹스인을 적용할 때는 with 키워드 사용
   class BoyGroup extends Idol with IdolSingMixin{
-    BoyGroup(super.name, super.@override
-  // TODO: implement membersCount
-  int get membersCount => super.membersCount;)
+    BoyGroup(super.name, super.membersCount,);
   }
+
+  void sayMale(){
+    print('저는 남자 아이돌입니다.');
+  }
+
+  void main(){
+    BoyGroup bts = BoyGroup('BTS', 7);
+  }
+
+  
 }
