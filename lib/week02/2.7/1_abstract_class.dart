aab8e8700037f106cea81a5e20e8ffe8ac58dcc2
savic116 <savic00085@gmail.com>
2025-09-09 15:12:23 +0900

    feat: Implement sayName and sayMembersCount methods in GirlGroup

--- lib/week02/2.7/1_abstract_class.dart
+++ lib/week02/2.7/1_abstract_class.dart
@@ -16,5 +16,12 @@ class GirlGroup implements Idol{
 
 
   GirlGroup(this,name, this.membersCount);
-  
+
+  void sayName(){
+    print('저는 여자 아이돌 ${this.name}입니다.');
+  }
+
+  void sayMembersCount(){
+    print('${this.name} 맴버는 ${this.membersCount}명입니다.');
+  }
 }
