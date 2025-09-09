f0f1527bca0211fb633bd5136ddd751b839f7b2c
savic116 <savic00085@gmail.com>
2025-09-09 13:56:22 +0900

    feat: Implement Idol class with name and membersCount properties

--- lib/week01/2.3/1_inheritance.dart
+++ lib/week01/2.3/1_inheritance.dart
@@ -1,4 +1,13 @@
 class Idol {
   final String name;
-  
+  final int membersCount;
+
+  Idol(this.name, this.membersCount);
+
+  void sayName(){
+    print('저는 ${this.name}입니다.');
+  }
+
+
+  void sayMembers
 }
