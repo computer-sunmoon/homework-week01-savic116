d3e40bc06e057194405d8e9641168cfca3dc09f7
savic116 <savic00085@gmail.com>
2025-09-09 15:05:53 +0900

    feat(mixin): Implement Idol class and IdolSingMixin

--- lib/week02/2.6/1_mixin.dart
+++ lib/week02/2.6/1_mixin.dart
@@ -1,5 +1,19 @@
 import 'package:myapp/week02/2.3/1_inheritance.dart';
+class Idol {
+  final String name;
+  final int membersCount;
 
+  Idol(this.name, this.membersCount);
+
+  void sayName(){
+    print('저는 ${this.name}입니다.');
+  }
+
+
+  void sayMembersCount(){
+    print('${this.name} 맴버는 ${this.membersCount}명입니다.');
+  }
+}
 mixin IdolSingMixin on Idol{
   void sing(){
     print('${this.name}이 노래를 부릅니다.');
