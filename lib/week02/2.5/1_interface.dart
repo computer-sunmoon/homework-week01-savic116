58309bd4b214ac31ad5118b0c75e4797925d5b05
savic116 <savic00085@gmail.com>
2025-09-09 15:00:53 +0900

    feat: Implement and use Idol interface with GirlGroup class

--- lib/week02/2.5/1_interface.dart
+++ lib/week02/2.5/1_interface.dart
@@ -30,3 +30,10 @@ class GirlGroup implements Idol{
     print('${this.name} 맴버는 ${this.membersCount}명입니다.');
   }
 }
+
+void main(){
+  GirlGroup blackPink = GirlGroup('블랙핑크', 4);
+
+  blackPink.sayName();
+  blackPink.sayMembersCount();
+}
