6510f8e166bfa39c192e534baa8fb7eb7b27f152
savic116 <savic00085@gmail.com>
2025-09-10 12:16:42 +0900

    Fix: Calculate and print sum in addNumbers

--- lib/week02/3.2/1_2_future.dart
+++ lib/week02/3.2/1_2_future.dart
@@ -7,6 +7,6 @@ void addNumbers(int number1, int number2){
 
   // Future.delayed()를 사용하면 일정 시간 후에 콜백 함수를 실행할 수 있음
   Future.delayed(Duration(seconds: 3), (){
-    
-  }
+    print('$number1 + $number2 = ${number1 + number2}');
+  };
 }
