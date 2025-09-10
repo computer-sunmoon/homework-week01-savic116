4565161882bf0f747582d895f623731fe6fca111
savic116 <savic00085@gmail.com>
2025-09-10 12:20:12 +0900

    feat: Implement basic async function `addNumbers`

--- lib/week02/3.3/1_1_async_and_await.dart
+++ lib/week02/3.3/1_1_async_and_await.dart
@@ -1,1 +1,11 @@
-void main(){}
+void main(){
+  addNumbers(1, 1);
+}
+
+//async 키워드는 함수 매개변수 정의와 바디 사이에 입력합니다.
+Future<void> addNumbers(int number1, int number2) async{
+  print('$number1 + $number2 계산 시작!');
+
+
+  // await은 대기하고 싶은 비동기 함수 앞에 입력합니다.
+}
