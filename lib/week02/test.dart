968e6c344e33b852ff22dea006b4a0fdab42b741
savic116 <savic00085@gmail.com>
2025-09-11 15:05:10 +0900

    Fix: Specify parameter type and remove unused method.

--- lib/week02/test.dart
+++ lib/week02/test.dart
@@ -34,9 +34,10 @@ class Idol {
       print("멤버 수는 1~12 사이여야 합니다.");
     }
   }
-  sayName(name){
+  sayName(String name){
     print("저는 ${name}입니다");
   }
+  printSummary()
 }
 void main(){
   Idol blackPink = Idol();
