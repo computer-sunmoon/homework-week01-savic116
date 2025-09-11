632441b0525f0a920757e93b35b51e67899ab069
savic116 <savic00085@gmail.com>
2025-09-11 14:43:11 +0900

    feat(week02/5): Add comments explaining MaterialApp, Scaffold, and Text widgets.

--- lib/week02/5/main.dart
+++ lib/week02/5/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Text(
-          'Hello Code Factory',
+    MaterialApp(     // 머터리얼 디자인 위젯
+      home: Scaffold(  // Scaffold 위젯
+        body: Text( // text 위젯
+          'Hello Code Factory', // 마지막 매개변수 끝에 콤마 추가
         ),
       ),
-    )
+    ),
   );
 }
