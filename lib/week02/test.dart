a01ac15f1c409ec861e277c5bd024d81ad7d1795
savic116 <savic00085@gmail.com>
2025-09-11 15:02:10 +0900

    Fix: Corrected Idol class member getter and setter types

--- lib/week02/test.dart
+++ lib/week02/test.dart
@@ -18,7 +18,7 @@ class Idol {
   // get 키워드를 사용해서 게터임을 명시합니다.
   // 게터는 메서드와 다르게 매개변수를 전혀 받지 않습니다.
 
-  String get members {
+  int get members {
     
     return this._members;
   }
@@ -26,8 +26,9 @@ class Idol {
   // 세터는 set이라는 키워드를 사용해서 선언합니다.
   // 세터는 매개번수로 딱 하나의 변수를 받을 수 있습니다.
 
-  set name(String name){
-    this._name = name;
+  set members(int members){
+    if()
+    this._members = members;
   }
 }
 void main(){
