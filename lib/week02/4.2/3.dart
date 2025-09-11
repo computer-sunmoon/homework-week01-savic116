b20fdb4250b556c61e6941f4661e3a03e6c53dae
savic116 <savic00085@gmail.com>
2025-09-11 13:18:10 +0900

    Fix: Destructure 'age' in map assignment

--- lib/week02/4.2/3.dart
+++ lib/week02/4.2/3.dart
@@ -1,5 +1,8 @@
 void main(){
   final minjiMap = {'name': '민지', 'age' : 19};
   // 위의 맵의 구조와 똑같은 구조로 구조 분해하면 됩니다.
-  final {'name' : name, 'age'}
+  final {'name' : name, 'age' : age} = minjiMap;
+
+  // name : 민지
+  print('name': $name)
 }
