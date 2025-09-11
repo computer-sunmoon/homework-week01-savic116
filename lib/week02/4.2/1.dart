33d868c52abc1b1b2055f65b6231fb9cb2dd2f95
savic116 <savic00085@gmail.com>
2025-09-11 13:13:39 +0900

    feat: Demonstrate list destructuring in Dart

--- lib/week02/4.2/1.dart
+++ lib/week02/4.2/1.dart
@@ -3,5 +3,10 @@ void main(){
   // final newJeans = ['민지', '해린'];
   // final minji = newJeans[0];
   // final haerin = newJeans[1];
-  final [minji, haerin]
+  final [minji, haerin] = ['민지', '해린'];
+
+  // 민지 출력
+  print(minji);
+  // 해린 출력
+  print(haerin);
 }
