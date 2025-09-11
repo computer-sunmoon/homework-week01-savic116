1af780643d217a182f5452c13d0c4980e499560e
savic116 <savic00085@gmail.com>
2025-09-11 14:11:10 +0900

    feat: Add more test cases to switcher function

--- lib/week02/4.3/2.dart
+++ lib/week02/4.3/2.dart
@@ -27,5 +27,14 @@ void switcher(dynamic anything){
 void main(){
   // match: aaa 출력
   switcher('aaa');
-
+  // match: [1, 2] 출력
+  switcher([1, 2]);
+  // match: [_, _, _]출력
+  switcher([3, 4, 5]);
+  // match: [int 6, int 7] 출력
+  switcher([6, 7]);
+  // match: (String: 민지, int: 19) 출력
+  switcher(('민지', 19));
+  // no match 출력
+  switcher(8);
 }
