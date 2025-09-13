72101340097af612559ac524f1a41b77051a5bbb
savic116 <savic00085@gmail.com>
2025-09-13 22:58:02 +0900

    Fix: Print calculation result in `addNumbers` function

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -9,6 +9,9 @@ Future<int> addNumbers(int number1, int number2) async {
   print('$number1 + $number2 계산 시작!');
 
   await Future.delayed(Duration(seconds: 3), (){
-    print
-  })
+    print('$number1 + $number2 = ${number1 + number2}');
+  });
+
+  print('$number1 + $number2 코드 실행 끝');
+  return number1 + number2;
 }
