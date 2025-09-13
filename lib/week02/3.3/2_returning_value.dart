e29476bac7ec678c9d743eee14ea34c85917a288
savic116 <savic00085@gmail.com>
2025-09-13 22:56:02 +0900

    feat: Implement addNumbers function with Future<int> return type

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -1,6 +1,10 @@
 void main() async{
-  final result = await addNumber(1, 1);
+  final result = await addNumbers(1, 1);
   print('결괏값 $result'); // 일반 함수와 동일하게 반환값을 받을 수 있음
   final result2 = await addNumbers(2, 2);
   print('결괏값 $result2');
+}
+
+Future<int> addNumbers(int number1, int number2) async {
+  
 }
