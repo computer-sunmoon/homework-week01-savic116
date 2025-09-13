9ec1047686813901efecfc29fbf0e090f23b349a
savic116 <savic00085@gmail.com>
2025-09-13 22:23:01 +0900

    feat: Add Idol class and basic usage example

--- lib/week01/2.2/1_class.dart
+++ lib/week01/2.2/1_class.dart
@@ -14,3 +14,13 @@ class Idol{
     print('저는 $name입니다.');
   }
 }
+void main(){
+  // 변수 타입을 Idol로 지정하고
+  // Idol의 인스턴스를 생성할 수 있습니다.
+  // 인스턴스를 생성할 때는 함수를 실행하는 것처럼
+  // 인스턴스화하고 싶은 클래스에 괄호를 열고 닫아줍니다.
+  Idol blackPink = Idol(); // Idol 인스턴스 생성
+
+  // 메서드를 실행합니다.
+  blackPink.sayName();
+}
