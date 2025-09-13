ce704658cf4940bd87b8051b8671c3483ab16a48
savic116 <savic00085@gmail.com>
2025-09-13 22:57:02 +0900

    feat: Add `addNumbers` function with delay and logging

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -6,5 +6,9 @@ void main() async{
 }
 
 Future<int> addNumbers(int number1, int number2) async {
-  
+  print('$number1 + $number2 계산 시작!');
+
+  await Future.delayed(Duration(seconds: 3), (){
+    print
+  })
 }
