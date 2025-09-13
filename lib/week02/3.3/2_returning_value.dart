005a1f80a21098fd5928d8989267a87e6b8cc582
savic116 <savic00085@gmail.com>
2025-09-13 22:55:02 +0900

    feat(week02): Demonstrate async function return values

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -1,3 +1,6 @@
 void main() async{
-  
+  final result = await addNumber(1, 1);
+  print('결괏값 $result'); // 일반 함수와 동일하게 반환값을 받을 수 있음
+  final result2 = await addNumbers(2, 2);
+  print('결괏값 $result2');
 }
