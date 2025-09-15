e3edfb079a4fde4f2eb6c443aed4732c235054c1
savic116 <savic00085@gmail.com>
2025-09-15 09:49:32 +0900

    Fix: Add Text widget and remove duplicate body

--- lib/week02/5/main.dart
+++ lib/week02/5/main.dart
@@ -4,10 +4,10 @@ void main(){
     MaterialApp(     // 머터리얼 디자인 위젯
       home: Scaffold(  // Scaffold 위젯
         body: Center(
-          
-        )
-        body: Text( // text 위젯
+           body: Text( // text 위젯
           'Hello Code Factory', // 마지막 매개변수 끝에 콤마 추가
+        ),
+
         ),
       ),
     ),
