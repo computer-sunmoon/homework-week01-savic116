ca37185c35de7e707f2d78842527cbce9af9360a
savic116 <savic00085@gmail.com>
2025-09-15 10:37:36 +0900

    feat: Comment out unused Future variable declarations and cascade operator example

--- lib/week02/2.10/1_cascade_operator.dart
+++ lib/week02/2.10/1_cascade_operator.dart
@@ -1,8 +1,8 @@
 void main(){
   // casecade operator(...)을 사용하면
   // 선언한 변수의 메서드를 연속으로 실행할 수 있습니다.
-  Idol blackPink = Idol('블랙핑크', 4)
-   ..sayName()
+//  Idol blackPink = Idol('블랙핑크', 4)
+ //  ..sayName()
    ..sayMembersCount();
   
 }
