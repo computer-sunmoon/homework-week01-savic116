ff38da487bfa9f1a181d9f07de599d0fd69527f0
savic116 <savic00085@gmail.com>
2025-09-16 13:34:38 +0900

    Fix: Set TextButton foregroundColor to red

--- lib/week03/text_button_widget.dart
+++ lib/week03/text_button_widget.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget{
           child: TextButton(
             onPressed: () {},
             style: TextButton.styleFrom(
-              for
+              foregroundColor: Colors.red,
             ),
           ),
       ),
