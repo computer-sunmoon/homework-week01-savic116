8fe439c9112f81527add1e514e22645f566144a0
savic116 <savic00085@gmail.com>
2025-09-16 14:50:41 +0900

    feat: Add padding to the inner container widget

--- lib/week03/padding_margin_widget.dart
+++ lib/week03/padding_margin_widget.dart
@@ -21,9 +21,9 @@ class MyApp extends StatelessWidget{
                 padding: EdgeInsetsGeometry.all(16.0),
 
                 child: Container(
-              color: Colors.red,
-              width: 50.0,
-              height: 50.0,
+                  color: Colors.red,
+                  width: 50.0,
+                  height: 50.0,
                  ),
                 ),     
             ),
