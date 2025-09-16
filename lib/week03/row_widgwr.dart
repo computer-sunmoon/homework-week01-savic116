6187ed132bb6ee4af8d5f3eb53b3c393b37b0f8c
savic116 <savic00085@gmail.com>
2025-09-16 15:10:21 +0900

    feat: Add a green container with spacing to the Row example.

--- lib/week03/row_widgwr.dart
+++ lib/week03/row_widgwr.dart
@@ -25,6 +25,14 @@ class RowWidgetExample extends StatelessWidget{
                 width: 50.0,
                 color: Colors.red,
               ),
+              // SizedBox는 일반적으로 공백을
+              // 생성할 때 사용
+              const SizedBox(width: 12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.green,
+              )
             ]
           )
           
