9750848bd4f71adbfa3cc5096fb18875bdb740df
savic116 <savic00085@gmail.com>
2025-09-16 14:33:41 +0900

    feat: Remove unused Icon widget from Container example.

--- lib/week03/container_widget.dart
+++ lib/week03/container_widget.dart
@@ -27,9 +27,7 @@ class MyApp extends StatelessWidget{
                 16.0,
               ),
             ),
-            icon: Icon(
-              Icons.home,
-            ),
+            
           ),
       ),
     ),
