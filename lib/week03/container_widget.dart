c6c75a346a4dbce5573a024bfe1583a4bc9e6ca7
savic116 <savic00085@gmail.com>
2025-09-16 14:30:11 +0900

    feat: Replace FAB with IconButton in ContainerWidget

--- lib/week03/container_widget.dart
+++ lib/week03/container_widget.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(FloatingActionButtonExample());
+  runApp(MyApp());
 }
 
-class FloatingActionButtonExample extends StatelessWidget{
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          //클릭했을 때 실행할 함수
+        body: Center(
+          child: IconButton(
             onPressed: () {},
-            child: Text('클릭'),
+            icon: Icon(
+              Icons.home,
+            ),
           ),
-          body: Container(),
       ),
+    ),
    );
   }
 }
