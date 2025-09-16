e2957803ab98d3172f39392fe50fecd0a9a3ef7a
savic116 <savic00085@gmail.com>
2025-09-16 14:14:41 +0900

    feat: Add onTap functionality to GestureDetector

--- lib/week03/gesture_detector_widget.dart
+++ lib/week03/gesture_detector_widget.dart
@@ -13,11 +13,10 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         body: Center(
           GestureDetector(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.red,
-            ),
-          child: Text('엘리베이티드버튼'),
+            onTap:(){
+              // 출력 결과는 안드로이드 스튜디오의 {run}탭에서 확인 가능합니다.
+              print('on tap');
+            }
           ),
       ),
     ),
