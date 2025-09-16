52cc97213d222f065f1d1be41042a4de34054cdf
savic116 <savic00085@gmail.com>
2025-09-16 14:12:41 +0900

    feat: Add basic ElevatedButton example in GestureDetectorWidget

---
+++ lib/week03/gesture_detector_widget.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+          child: Text('엘리베이티드버튼'),
+          ),
+      ),
+    ),
+   );
+  }
+}
