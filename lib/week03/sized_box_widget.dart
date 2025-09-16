0803c0d4d2e283f6d8e763f5fb6207fd0e64bdb2
savic116 <savic00085@gmail.com>
2025-09-16 14:38:52 +0900

    feat(week03): Implement SizedBox with fixed dimensions and background color

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -11,9 +11,14 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         body: Center(
           child: SizedBox(
-            onPressed: () {},
-            icon: Icon(
-              Icons.home,
+            // 높이 지정
+            height: 200.0,
+            // 너비 지정
+            width: 200.0,
+            // SizeBox는 색상이 없으므로 크기를 확인하는
+            // 용도로 Container 추가
+            child: Container(
+              color: Colors.red,
             ),
           ),
       ),
