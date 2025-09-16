40eed2f9ca82f161f160d058e1a0ce648ace7c9f
savic116 <savic00085@gmail.com>
2025-09-16 14:41:41 +0900

    feat: Replace SizedBox with Container in padding_widget

--- lib/week03/padding_widget.dart
+++ lib/week03/padding_widget.dart
@@ -10,13 +10,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: SizedBox(
-            // 높이 지정
-            height: 200.0,
-            // 너비 지정
-            width: 200.0,
-            // SizeBox는 색상이 없으므로 크기를 확인하는
-            // 용도로 Container 추가
+          child: Container(co
             child: Container(
               color: Colors.red,
             ),
