1f62d5ec1035a4e4ea96c8d0772f68e650347193
savic116 <savic00085@gmail.com>
2025-09-16 15:04:52 +0900

    feat: Add basic container widget to row_widgwr.dart

---
+++ lib/week03/row_widgwr.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          top: true,
+          child: Container(
+            color: Colors.red,
+            height: 100.0,
+            width: 100.0,
+          )
+          
+        ),
+      ),
+    );
+  }
+}
