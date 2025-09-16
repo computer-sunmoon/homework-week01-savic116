fc897f9532daa46f6cc05e0dd65986c69bcc602a
savic116 <savic00085@gmail.com>
2025-09-16 14:08:11 +0900

    Fix: Use `Icons.home` instead of `Icon.home`

--- lib/week03/icon_button_widget.dart
+++ lib/week03/icon_button_widget.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget{
           child: IconButton(
             onPressed: () {},
             icon: Icon(
-              Icon.home,
+              Icons.home,
             ),
           ),
       ),
@@ -24,10 +24,3 @@ class MyApp extends StatelessWidget{
   }
 }
 
-IconButton(
-  onPressed:() {},
-  icon: Icon(
-    // 플러터에서 기본으로 제공하는 아이콘입니다.
-    Icon.home,
-  ),
-)
