4175a1178ac69b3350a8470d6c4b33ea053b8376
savic116 <savic00085@gmail.com>
2025-09-16 14:05:10 +0900

    feat: Implement basic ElevatedButton example in icon_button_widget.dart

--- lib/week03/icon_button_widget.dart
+++ lib/week03/icon_button_widget.dart
@@ -1,7 +1,26 @@
-IconButton(
-  onPressed:() {},
-  icon: Icon(
-    // 플러터에서 기본으로 제공하는 아이콘입니다.
-    Icon.home,
-  ),
-)
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+          child: Text('엘리베이티드버튼'),
+          ),
+      ),
+    ),
+   );
+  }
+}
