750560dbec46d455cc71faf4a769a465ed0768e5
savic116 <savic00085@gmail.com>
2025-09-16 13:14:04 +0900

    feat: Add basic Text widget to week03

---
+++ lib/week03/text_button_widget.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            '코트팩토리',
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+              color: Colors.blue,
+            ),
+          ),
+      ),
+    ),
+   );
+  }
+}
