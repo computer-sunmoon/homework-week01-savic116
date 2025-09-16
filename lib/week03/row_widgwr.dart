0e2dac1a1c06faf0813ce491524fbc8686b17dd7
savic116 <savic00085@gmail.com>
2025-09-16 15:06:21 +0900

    Fix: Use Row widget and set height to infinity for proper rendering

--- lib/week03/row_widgwr.dart
+++ lib/week03/row_widgwr.dart
@@ -10,8 +10,9 @@ class RowWidgetExample extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: SizedBox(
-          top: true,
-          child: Container(
+          // 반대축에서 이동한 공간을 제공하기 위해 높이를 최대한으로 설정
+          height: double.infinity,
+          child: Row(
             color: Colors.red,
             height: 100.0,
             width: 100.0,
