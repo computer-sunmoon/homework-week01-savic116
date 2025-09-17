4d3805e2068c0c8d8601ee2ed8e465f7b5ca01e3
savic116 <savic00085@gmail.com>
2025-09-17 11:55:51 +0900

    feat: Add basic ColumnWidgetExample

---
+++ lib/week03/column_widget.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(ColumnWidgetExample());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+      ),
+    ),
+   );
+  }
+}
