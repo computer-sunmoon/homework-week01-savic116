1d7e1f8b55ced0846adc78d6a27cba49512e6a0f
savic116 <savic00085@gmail.com>
2025-09-17 12:02:21 +0900

    feat: Implement basic Column widget example

--- lib/week03/column_widget.dart
+++ lib/week03/column_widget.dart
@@ -16,7 +16,32 @@ class ColumnWidgetExample extends StatelessWidget{
           width: double.infinity,
           child: Column(
             // 주축 정렬 지정
-            mainAxisAlignment: MainAxisAlignment,
+            mainAxisAlignment: MainAxisAlignment.start,
+
+            // 반대축 정렬 지정
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            // 넣고 싶은 위젯 입력
+            children: [
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.red,
+              ),
+              // SizedBox는 일반적으로 공백을 생성할 때 사용
+              const SizedBox(width: 12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.green,
+              ),
+              const SizedBox(width: 12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.blue,
+              )
+            ]
           )
           ),
       ),
