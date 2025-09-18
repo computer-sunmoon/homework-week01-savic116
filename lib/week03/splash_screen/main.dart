5bd309718d5c87937332aafe49f68b41a0c06185
savic116 <savic00085@gmail.com>
2025-09-18 12:57:32 +0900

    Fix: Corrected typo in SplashScreen widget call and added import.

--- lib/week03/splash_screen/main.dart
+++ lib/week03/splash_screen/main.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/material.dart';
 void main(){
-  runApp(splashScreen()); // SplashScreen 위젯을 첫 화면으로 지정
+  runApp(SplashScreen()); // SplashScreen 위젯을 첫 화면으로 지정
 }
 
 
@@ -8,6 +9,6 @@ class SplashScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    
+
   }
 }
