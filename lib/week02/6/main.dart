f5a7daeb26d38201651ae774b41bd72b7c2fbf62
savic116 <savic00085@gmail.com>
2025-09-19 23:52:30 +0900

    feat(week02/6): Add placeholder comment for example code

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: 
+          child:  // 여기에 예제 코드 작성하기
           ),
       ),
     ),
