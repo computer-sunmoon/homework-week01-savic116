397e2568dfd58457736dc36c7279be3a1b504bb9
savic116 <savic00085@gmail.com>
2025-09-20 00:02:00 +0900

    Fix: Replace Flexible with Expanded for blue container in ColumnWidgetExample.

--- lib/week03/expanded_widget.dart
+++ lib/week03/expanded_widget.dart
@@ -13,18 +13,17 @@ class ColumnWidgetExample extends StatelessWidget{
       home: Scaffold(
         body: Column(
           
-            // 파란색 Container
+            
             children: [
-              Flexible(
-                // flex는 남은 공간을 차지할 비율을 의미합니다.
-                // flex값을 제공하지 않으면 기본값은 1입니다.
-                flex: 1,
-              
+              // 파란색 Container
+              Expanded(
                 child: Container(
                   
                   color: Colors.blue,
                 ),
               ),
+
+                
              Flexible(
               flex: 1,
 
