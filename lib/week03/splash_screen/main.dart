243c07152d9345e47a4db6d2b10d47de000c36ce
savic116 <savic00085@gmail.com>
2025-09-20 00:30:31 +0900

    feat(splash_screen): Add CircularProgressIndicator with animation.

--- lib/week03/splash_screen/main.dart
+++ lib/week03/splash_screen/main.dart
@@ -25,7 +25,9 @@ class SplashScreen extends StatelessWidget{
               'assets/logo.png',
               width: 200,
             ),
-            CircularProgressIndicator(),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(),
+            ),
 
             ], 
           )
