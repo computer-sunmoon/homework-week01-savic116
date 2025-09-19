95a6d54bee093ba498fb41b7c0849d738cef3ed5
savic116 <savic00085@gmail.com>
2025-09-19 23:56:30 +0900

    feat: Implement basic text widget with styling

--- lib/week03/text_widget.dart
+++ lib/week03/text_widget.dart
@@ -1,8 +1,33 @@
-Text(
+
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(Text(
   // 작성하고 싶은 글
   '코드팩토리',
   // 글자에 스타일 적용
   style: TextStyle(
-    
+    // 글자 크기
+    fontSize: 16.0,
+    // 글자 굵기
+    fontWeiget: FontWeiget.w700,
+    // 글자 색상
+    color: Color.bule,
   )
 )
+          ),
+      ),
+    ),
+   );
+  }
+}
