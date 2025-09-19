7ecbafd48c92e914cf86ac7b84a4bc08948badd5
savic116 <savic00085@gmail.com>
2025-09-19 23:46:00 +0900

    Fix: Corrected `SizeBox` typo and centered column content.

--- lib/week02/6/children.dart
+++ lib/week02/6/children.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
+
 void main(){
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: SizeBox(
+        body: SizedBox(
           width: double.infinity,
           child: Column(
-            
+            mainAxisAlignment: MainAxisAlignment.center,
           )
         )
       )
