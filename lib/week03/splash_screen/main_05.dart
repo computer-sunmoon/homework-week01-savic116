9658b08193c405ddf2980fc129ce2eaf1d67cf53
savic116 <savic00085@gmail.com>
2025-09-21 13:32:57 +0900

    feat: Implement basic splash screen UI

---
+++ lib/week03/splash_screen/main_05.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(SplashScreen()); // SplashScreen 위젯을 첫 화면으로 지정
+
+}
+
+class SplashScreen extends StatelessWidget{  // StatelessWidget 선언
+  @override
+  Widget build(BuildContext context){ 
+    return MaterialApp(
+      home: Scaffold(
+        body: Container( // 컨테이너 위
+          // 컨테이너를 디자인하는 클래스
+          decoration: BoxDecoration(
+            color: Colors.orange,
+          ),
+
+          child: Center(
+            child: Text('Splash Screen'),
+          ),
+        ),
+      ),
+    );
+ 
+  }
+}
